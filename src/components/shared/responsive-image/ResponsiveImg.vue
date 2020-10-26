6a465f13d201a6074ce5dd4de374bf3665190731
atila-tokyo <atilamanger@gmail.com>
2020-10-26 23:13:13 +0900

    adding components

---
+++ src/components/shared/responsive-image/ResponsiveImg.vue
@@ -0,0 +1,17 @@
+<template>
+    <img class="responsive-img" :src="url" :alt="titulo">
+</template>
+
+<script>
+export default {
+
+    props: ['url', 'titulo']
+}
+</script>
+
+<style scoped>
+
+.responsive-img {
+    width: 100%;
+}
+</style>
---
+++ src/components/subscription/Subscription.vue
@@ -0,0 +1,13 @@
+<template>
+    <h2 class="centralized">Cadastros</h2>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+    .centralized {
+        text-align: center;
+    }
+</style>
