e49bd13c0327e8986c66660ff24b6a35d7155376
atila-tokyo <atilamanger@gmail.com>
2020-10-30 21:53:51 +0900

    Creating menu componennt

---
+++ src/components/shared/menu/Menu.vue
@@ -0,0 +1,19 @@
+<template>
+    <nav>
+      <ul>
+        <li v-for="route in routes" v-bind:key=route>
+          <router-link :to="route.path ? route.path : '/'">{{ route.title }}</router-link>
+        </li>
+      </ul>
+    </nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
